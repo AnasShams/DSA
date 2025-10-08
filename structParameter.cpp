c50cfc12795eee6dcd8af91c297597b4c30a20df
Anas Shams <anasshams1960@gmail.com>
2025-10-08 19:20:37 +0530

    Day3.2

--- structParameter.cpp
+++ structParameter.cpp
@@ -7,14 +7,16 @@ struct Rectangle{
     int breadth;
 };
 
-int area(struct Rectangle r1){
-    r1.length++;
-    return r1.length * r1.breadth;
+void fun(struct Rectangle *p){
+    p->length=20;
+    cout<<"Length"<<p->length<<endl<<"Breadth"<<p->breadth<<endl;
 }
 
 int main()
 {
     struct Rectangle r1={10,5};
-    cout<<area(r1)<<endl;
+    cout<<r1.length<<endl<<r1.breadth<<endl;
+    fun(&r1);
+    cout<<r1.length<<endl<<r1.breadth<<endl;
     return 0;
 }
